5f2f69cb243869e2e5069d0f5abfc2e8306d799a
gichanGim <k.kychan2003@gmail.com>
2025-12-29 01:58:05 +0900

    init commit

---
+++ src/main/java/com/example/loopitbe/LoopitBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loopitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopitBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loopitbe/LoopitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loopitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
