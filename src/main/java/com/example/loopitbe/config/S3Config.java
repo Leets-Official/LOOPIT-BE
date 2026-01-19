24d51973b940cccbb89c73a2cef4c53801a3bba7
gichanGim <k.kychan2003@gmail.com>
2026-01-19 19:13:04 +0900

    .env.test에서 직접 S3Config로 주입하게 변경

--- src/main/java/com/example/loopitbe/config/S3Config.java
+++ src/main/java/com/example/loopitbe/config/S3Config.java
@@ -10,10 +10,10 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${AWS_ACCESS_KEY}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${AWS_SECRET_KEY}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
