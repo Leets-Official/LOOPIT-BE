257a2b30042837dd44e4f38bac10864f48c58e4c
gichanGim <k.kychan2003@gmail.com>
2026-01-26 19:16:57 +0900

    작업 내용 저장

--- src/main/java/com/example/loopitbe/controller/SearchController.java
+++ src/main/java/com/example/loopitbe/controller/SearchController.java
@@ -1,10 +1,16 @@
 package com.example.loopitbe.controller;
 
+import com.example.loopitbe.common.ApiResponse;
 import com.example.loopitbe.service.S3Service;
 import com.example.loopitbe.service.SearchService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "Search", description = "검색 관련 컨트롤러")
 @RestController
@@ -14,5 +20,11 @@ public class SearchController {
 
     public SearchController(SearchService service) {
         this.service = service;
+    }
+
+    @GetMapping("/autocomplete")
+    public ResponseEntity<ApiResponse<List<String>>> autocomplete(@RequestParam(name = "keyword") String keyword) {
+        List<String> results = service.getAutocomplete(keyword);
+        return ResponseEntity.ok(ApiResponse.ok(results, "Auto complete"));
     }
 }
--- src/main/java/com/example/loopitbe/service/SearchService.java
+++ src/main/java/com/example/loopitbe/service/SearchService.java
@@ -1,8 +1,21 @@
 package com.example.loopitbe.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SearchService {
+    @Transactional
+    public List<String> getAutocomplete(String keyword) {
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return List.of();
+        }
 
+        return shopRepository.findTop10ByShopNameStartingWith(keyword)
+                .stream()
+                .map(Shop::getShopName)
+                .toList();
+    }
 }
